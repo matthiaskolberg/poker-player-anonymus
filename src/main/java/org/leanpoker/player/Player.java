bc66b86d04072b060f03f8d2eba8324a66336448
QS-EB744 <QS-EB744@qs-eb744s-mbp.fritz.box>
2017-12-14 10:12:37 +0100

    rock n roll

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,9 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+    	
+    		
+        return 10;
     }
 
     public static void showdown(JsonElement game) {
