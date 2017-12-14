c2a543b8f935cd6ccd6e8f215214715ac8055929
QS-EB744 <QS-EB744@qs-eb744s-mbp.fritz.box>
2017-12-14 10:25:07 +0100

    aha

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,19 +1,24 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+	static final String VERSION = "Default Java folding player";
 
-    public static int betRequest(JsonElement request) {
-    	
-   
-        return 50; 
-    }
+	public static int betRequest(JsonElement request) {
 
-    public static void showdown(JsonElement game) {
-    }
+		try {
+			JsonArray req = request.getAsJsonArray();
+			return 50;
+		} catch (Throwable e) {
+			return 50;
+		}
+	}
+
+	public static void showdown(JsonElement game) {
+	}
 }
